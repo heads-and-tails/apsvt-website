963510c4cd883f92f2ac5754a7028d86a3787f1b
Victoria Portnaya <victoriaportnaya@Victorias-MacBook-Air-6.local>
2026-09-11 14:16:40 +0300

    Use public source archive for existing programme documents

--- lib/programme-documents.ts
+++ lib/programme-documents.ts
@@ -1,2 +1,2 @@
 export const officialProgrammeFileBase =
-  "https://raw.githubusercontent.com/victoriaportnaya/apsvt-academy-website/1f82f55e259f8fe2d2acbddb7d36b6b303d2ef5a/public/documents/programmes/2025";
+  "https://raw.githubusercontent.com/heads-and-tails/apsvt-website/1f82f55e259f8fe2d2acbddb7d36b6b303d2ef5a/public/documents/programmes/2025";
